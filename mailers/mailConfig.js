a010b47ea8d684cfbd487f91fed088f444c95a07
devFresher <fresher.dev01@gmail.com>
2023-02-05 22:35:57 +0100

    fixing

--- mailers/mailConfig.js
+++ mailers/mailConfig.js
@@ -23,12 +23,12 @@ export const oauth2Transporter = async () => {
         oauth2Config.redirect_uri
     )
 
-    // throw({status: "error", code:401, message: await getAuthorizeUrl(oauth2Client)});
-    // return
-    const token = await oauth2Client.getToken(oauth2Config.auth_code)
-    // oauth2Client.setCredentials({refresh_token: oauth2Config.refresh_token})
-    // const token = await oauth2Client.getAccessToken()
-    throw(token);
+    throw({status: "error", code:401, message: await getAuthorizeUrl(oauth2Client)});
+    return
+    // const token = await oauth2Client.getToken(oauth2Config.auth_code)
+    oauth2Client.setCredentials({refresh_token: oauth2Config.refresh_token})
+    const token = await oauth2Client.getAccessToken()
+    // throw(token);
 
     return nodemailer.createTransport({
         service: 'gmail',
