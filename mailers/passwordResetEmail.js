9bc27ceaa8840238ba37ebf7dcae7df178015bc9
devFresher <fresher.dev01@gmail.com>
2023-02-05 20:52:35 +0100

    fixing

--- mailers/passwordResetEmail.js
+++ mailers/passwordResetEmail.js
@@ -10,37 +10,42 @@ const templatePaths = {
 }
 
 export const sendEmail = async (user, type) => {
-    // Read the HTML template
-    const templatePath = templatePaths[type];
-    const template = fs.readFileSync(templatePath, 'utf-8');
-
-    let data = {
-        appName: config.get('app.name'),
-        user,
-    }
-
-    if (type === 'resetPassword') {
-        data = {
-            ...data,
-            resetCode: user.resetPasswordToken,
-            expiry: `${Math.round((user.resetTokenExpiry - Date.now()) / (1000 * 60))} minutes`
+    try {
+        // Read the HTML template
+        const templatePath = templatePaths[type];
+        const template = fs.readFileSync(templatePath, 'utf-8');
+    
+        let data = {
+            appName: config.get('app.name'),
+            user,
         }
+    
+        if (type === 'resetPassword') {
+            data = {
+                ...data,
+                resetCode: user.resetPasswordToken,
+                expiry: `${Math.round((user.resetTokenExpiry - Date.now()) / (1000 * 60))} minutes`
+            }
+        }
+    
+        // Render the HTML
+        const html = mustache.render(template, data);
+    
+        // setup email data with unicode symbols
+        const mailOptions = {
+            from: `"${config.get('app.name')}" <admin.dev01@gmail.com>`,
+            to: `${user.email}`,
+            subject:  `${type === 'resetPassword' ? 'Reset Your' : 'Successful Reset of'} ${config.get('app.name')} Password`,
+            html: html
+        };
+    
+        const transporter = await oauth2Transporter()
+    
+        // send mail with defined transport object
+        const info = await transporter.sendMail(mailOptions);
+        return info;
+        
+    } catch (error) {
+        throw(error)
     }
-
-    // Render the HTML
-    const html = mustache.render(template, data);
-
-    // setup email data with unicode symbols
-    const mailOptions = {
-        from: `"${config.get('app.name')}" <admin.dev01@gmail.com>`,
-        to: `${user.email}`,
-        subject:  `${type === 'resetPassword' ? 'Reset Your' : 'Successful Reset of'} ${config.get('app.name')} Password`,
-        html: html
-    };
-
-    const transporter = await oauth2Transporter()
-
-    // send mail with defined transport object
-    const info = await transporter.sendMail(mailOptions);
-    return info;
 }
