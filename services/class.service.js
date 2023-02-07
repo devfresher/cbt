5d1a5ece65cf5b398ed07b783ecd6daedf055b53
Usman Soliu <fresher.dev01@gmail.com>
2023-02-07 10:52:55 +0100

    fix class

--- services/class.service.js
+++ services/class.service.js
@@ -16,11 +16,13 @@ const myCustomLabels = {
     meta: 'paging',
 };
 
-export const getOneClass = async (filterQuery) => {
-    let theClass = await Class.findOne(filterQuery)
-    if (!theClass) throw { status: "error", code: 404, message: "Class not found" }
-
-    return theClass
+export const getOneClass = (filterQuery) => {
+    return new Promise(async (resolve, reject) => {
+        let theClass = await Class.findOne(filterQuery)
+        if (!theClass) reject({ status: "error", code: 404, message: "Class not found" })
+    
+        resolve(theClass)
+    })
 }
 
 export const getMany = async (filterQuery, pageFilter) => {
